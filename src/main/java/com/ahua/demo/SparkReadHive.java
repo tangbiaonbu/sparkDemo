7f9ba4522aab89b4e9bfb47944913b80421aedb4
tangbiaonbu <tangbiao_nbu@outlook.com>
2021-12-11 18:32:07 +0800

    [ADD] spark 操作 hive 数据库的demo.

--- src/main/java/com/ahua/demo/SparkReadHive.java
+++ src/main/java/com/ahua/demo/SparkReadHive.java
@@ -1,16 +1,22 @@
 package com.ahua.demo;
 
+import org.apache.spark.sql.SparkSession;
+
 /**
  * @author 唐少
  * @version 1.0
- * @description: spark 读取hive的表
+ * @description: spark 读取hive的表，读取hive的数据数据扫描
  * @date 2021/12/5 20:07
  */
 public class SparkReadHive {
     public static void main(String[] args) {
-
-
-
+        SparkSession sc = SparkSession.builder()
+                .enableHiveSupport()
+                .config("spark.sql.warehouse.dir", "hdfs://192.168.1.100:8020/user/hive/warehouse")
+                .getOrCreate();
+        sc.sql("use ahua");
+        sc.sql("select * from ahua.person").show();
+        sc.sql("select count(*) from ahua.person where ahua.person.name='tangbiao'").show();
+        sc.stop();
     }
-
 }
